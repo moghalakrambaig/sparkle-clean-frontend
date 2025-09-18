7fed605f212d48b0895f5fe64cc1c2b6c3ad505e
Moghal Akram Baig <moghalakrambaig8@gmail.com>
2025-09-18 12:57:45 +0530

    changes

--- App.tsx
+++ App.tsx
@@ -333,7 +333,7 @@ const ServicesPage = () => {
                         </div>
                         <p className="text-gray-600 flex-grow mb-6">{service.description}</p>
                         <button onClick={() => navigate(`/booking?service=${service.id}`)} className="mt-auto bg-sky-500 text-white px-6 py-2 rounded-full hover:bg-sky-600 transition duration-300 self-start">
-                            Book This Service
+                            Book This Servic
                         </button>
                     </div>
                 ))}
